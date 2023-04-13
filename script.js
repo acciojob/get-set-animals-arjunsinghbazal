452508cfa0d03bcd83ed6d40cfe6103e0788c555
Arjun Singh <55157623+arjunsinghbazal@users.noreply.github.com>
2023-04-13 13:16:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,13 +12,13 @@ class Animal {
 
 class Cat extends Animal {
 	purr(){
-		console.log("purr");
+		console.log('The Siamese makes a sound');
 	}
 }
 
 class Dog extends Animal {
 	bark(){
-		console.log("woof");
+		console.log('The Golden Retriever makes a sound');
 	}
 }
 let cat = new Cat("cat");
